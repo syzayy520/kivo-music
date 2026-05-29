9713191e4e9e0c85afb9347650a7f98916e7204d
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-29 23:32:13 +0800

    KIVO-MUSIC-V1-LISTEN-NOW add listen now composer

---
+++ src/features/listen-now/ListenNow.tsx
@@ -0,0 +1,13 @@
+import './listenNow.css'
+import '../../shared/styles/artwork.css'
+import { AlbumShelf } from './components/AlbumShelf'
+import { ListenNowHero } from './components/ListenNowHero'
+
+export function ListenNow() {
+  return (
+    <>
+      <ListenNowHero />
+      <AlbumShelf />
+    </>
+  )
+}
