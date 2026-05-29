5dfe276031655704d9902bc00f010f01c6871ab7
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-30 03:32:56 +0800

    KIVO-MUSIC-WINDOW-CHROME-P1-004 mount custom chrome

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,13 @@ import { ListenNow } from '../features/listen-now/ListenNow'
 import { PlayerBar } from '../features/player/PlayerBar'
 import { Sidebar } from '../features/sidebar/Sidebar'
 import { TopBar } from '../features/top-bar/TopBar'
+import { WindowChrome } from '../features/window-chrome/WindowChrome'
 import './appShell.css'
 
 export function App() {
   return (
     <main className="km-app">
+      <WindowChrome />
       <Sidebar />
       <section className="km-stage">
         <TopBar />
