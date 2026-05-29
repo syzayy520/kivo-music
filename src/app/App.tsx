4476ca6c69f11b82f5a78e24cf54f61d9d9debdb
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-30 03:07:29 +0800

    KIVO-MUSIC-HOME-SURFACE-IA-P1-003A focus home surface

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,4 @@
-import { AlbumDetail } from '../features/album-detail/AlbumDetail'
-import { LibraryOverview } from '../features/library/LibraryOverview'
 import { ListenNow } from '../features/listen-now/ListenNow'
-import { NowPlaying } from '../features/now-playing/NowPlaying'
 import { PlayerBar } from '../features/player/PlayerBar'
 import { Sidebar } from '../features/sidebar/Sidebar'
 import { TopBar } from '../features/top-bar/TopBar'
@@ -14,9 +11,6 @@ export function App() {
       <section className="km-stage">
         <TopBar />
         <ListenNow />
-        <AlbumDetail />
-        <LibraryOverview />
-        <NowPlaying />
       </section>
       <PlayerBar />
     </main>
