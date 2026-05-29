a94fbb5f0322150fcaf7d1f712197ad580c70b62
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-29 23:03:40 +0800

    KIVO-MUSIC-ALBUM-DETAIL add data boundary

---
+++ src/features/album-detail/albumDetailData.ts
@@ -0,0 +1,11 @@
+import { albums } from '../../data/mock/albums'
+import { artists } from '../../data/mock/artists'
+import { tracks } from '../../data/mock/tracks'
+
+const album = albums[0]
+
+export const albumDetailData = {
+  album,
+  artist: artists.find((item) => item.id === album.artistId),
+  tracks: tracks.filter((track) => track.albumId === album.id),
+}
