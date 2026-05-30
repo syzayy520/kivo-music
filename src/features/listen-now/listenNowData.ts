65a29220e5780787f52d38b5e03da9b721d875d3
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-30 11:01:26 +0800

    KIVO-MUSIC-HOME-DENSITY-P1-006D extend home shelf sequences

--- src/features/listen-now/listenNowData.ts
+++ src/features/listen-now/listenNowData.ts
@@ -2,13 +2,15 @@ import { albums } from '../../data/mock/albums'
 import { artists } from '../../data/mock/artists'
 
 const heroAlbum = albums[0]
+const homeShelfAlbums = [...albums, ...albums]
+const recentShelfAlbums = [...albums].reverse().concat(albums)
 
 export const listenNowData = {
   heroAlbum,
   heroArtist: artists.find((artist) => artist.id === heroAlbum.artistId),
   shelves: {
-    recentlyPlayed: albums,
-    recentlyAdded: [...albums].reverse(),
+    recentlyPlayed: homeShelfAlbums,
+    recentlyAdded: recentShelfAlbums,
   },
   artists,
 }
