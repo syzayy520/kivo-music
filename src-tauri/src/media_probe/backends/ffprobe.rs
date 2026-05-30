5b18602530fdd2a87ce20af56cb348ea69e9330e
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-31 01:23:32 +0800

    KIVO-MEDIA-PROBE-FFPROBE-P0-007E route backend through command runner

--- src-tauri/src/media_probe/backends/ffprobe.rs
+++ src-tauri/src/media_probe/backends/ffprobe.rs
@@ -1,10 +1,6 @@
-use std::process::Command;
-
-use serde_json::Value;
-
-use super::ffprobe_error::{command_error_message, spawn_error_message};
+use super::ffprobe_command::read_probe_json;
 use super::ffprobe_json::parse_probe_result;
-use super::super::service::{MediaProbeError, MediaProbeResultValue, ProbeBackend};
+use super::super::service::{MediaProbeResultValue, ProbeBackend};
 use super::super::types::MediaProbeResult;
 
 #[derive(Clone, Debug, Default)]
@@ -16,29 +12,7 @@ impl ProbeBackend for FfprobeBackend {
     }
 
     fn probe(&self, path: &str) -> MediaProbeResultValue<MediaProbeResult> {
-        let output = Command::new("ffprobe")
-            .args([
-                "-v",
-                "error",
-                "-print_format",
-                "json",
-                "-show_format",
-                "-show_streams",
-            ])
-            .arg(path)
-            .output()
-            .map_err(|error| MediaProbeError::BackendUnavailable(spawn_error_message(error)))?;
-
-        if !output.status.success() {
-            return Err(MediaProbeError::ProbeFailed(command_error_message(
-                &output.stderr,
-                "ffprobe command failed without stderr output",
-            )));
-        }
-
-        let root: Value = serde_json::from_slice(&output.stdout)
-            .map_err(|error| MediaProbeError::ProbeFailed(error.to_string()))?;
-
+        let root = read_probe_json(path)?;
         Ok(parse_probe_result(path, &root))
     }
 }
