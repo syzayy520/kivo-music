cf93fc2e786f6cb8d5f66f460e94e4f1b45bc53d
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-30 18:39:17 +0800

    remove unused listen now layout helper

--- src/features/listen-now/listenNowLayout.ts
+++
@@ -1,8 +0,0 @@
-import { resolvePlayerBarVisibility } from '../player/playerBarVisibility'
-import { playerData } from '../player/playerData'
-
-export function resolveListenNowLayoutState() {
-  return {
-    playerBarVisibility: resolvePlayerBarVisibility(playerData.state.sessionState),
-  } as const
-}
