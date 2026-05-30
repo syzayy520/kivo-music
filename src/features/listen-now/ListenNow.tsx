0604a00339774f2165773e59bfec64489109e90e
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-30 18:48:20 +0800

    KIVO-MUSIC-HOME-START-VISUAL-P1-020 remove start panel from home

--- src/features/listen-now/ListenNow.tsx
+++ src/features/listen-now/ListenNow.tsx
@@ -1,13 +1,11 @@
 import './listenNow.css'
 import '../../shared/styles/artwork.css'
 import { AlbumShelf } from './components/AlbumShelf'
-import { StartListeningPanel } from './components/StartListeningPanel'
 import { TopPicksShelf } from './components/TopPicksShelf'
 
 export function ListenNow() {
   return (
     <div className="km-listen-now">
-      <StartListeningPanel />
       <TopPicksShelf />
       <AlbumShelf shelfKey="recentlyPlayed" />
       <AlbumShelf shelfKey="recentlyAdded" />
