5b6875100c40dc67ed48e9ad8ee86962203d3511
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-31 11:10:32 +0800

    KIVO-MUSIC-FE-DESIGN-TOKENS-P0-003 apply listen now tokens

--- src/features/listen-now/ListenNow.tsx
+++ src/features/listen-now/ListenNow.tsx
@@ -2,10 +2,11 @@ import './listenNow.css'
 import '../../shared/styles/artwork.css'
 import { AlbumShelf } from './components/AlbumShelf'
 import { TopPicksShelf } from './components/TopPicksShelf'
+import { listenNowStyle } from './listenNowStyle'
 
 export function ListenNow() {
   return (
-    <div className="km-listen-now">
+    <div className="km-listen-now" style={listenNowStyle}>
       <TopPicksShelf />
       <AlbumShelf shelfKey="recentlyPlayed" />
       <AlbumShelf shelfKey="recentlyAdded" />
