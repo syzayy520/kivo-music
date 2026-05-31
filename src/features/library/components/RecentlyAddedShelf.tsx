af872d833c7f024a648962acb8530919538e0625
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-31 19:00:34 +0800

    KIVO-MUSIC-LIBRARY-RECENTLY-ADDED-SPLIT-P0-033 wire recently added shelf style

--- src/features/library/components/RecentlyAddedShelf.tsx
+++ src/features/library/components/RecentlyAddedShelf.tsx
@@ -1,8 +1,10 @@
 import { libraryData } from '../libraryData'
+import { recentlyAddedStyle } from './recentlyAddedStyle'
+import './recentlyAddedShelf.css'
 
 export function RecentlyAddedShelf() {
   return (
-    <div className="km-recently-added">
+    <div className="km-recently-added" style={recentlyAddedStyle}>
       {libraryData.recentlyAddedAlbums.map((album) => (
         <article className="km-recently-added-card" key={album.id}>
           <div className={`km-recently-added-art artwork-${album.artworkKey}`} />
