6a172bee2c81f3335713d2af8c13c38f889df5e2
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-31 10:52:27 +0800

    KIVO-MUSIC-FE-DESIGN-TOKENS-P0-003 apply app shell tokens

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,12 @@ import { PlayerBar } from '../features/player/PlayerBar'
 import { Sidebar } from '../features/sidebar/Sidebar'
 import { TopBar } from '../features/top-bar/TopBar'
 import { WindowChrome } from '../features/window-chrome/WindowChrome'
+import { appShellStyle } from './appShellStyle'
 import './appShell.css'
 
 export function App() {
   return (
-    <main className="km-app">
+    <main className="km-app" style={appShellStyle}>
       <WindowChrome />
       <Sidebar />
       <section className="km-stage">
