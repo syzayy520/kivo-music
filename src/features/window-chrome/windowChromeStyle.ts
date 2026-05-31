ebd7a4f3cd69e77393f98930f9b13d0edece6f4a
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-31 14:02:33 +0800

    KIVO-MUSIC-FE-WINDOW-CHROME-TOKENS-P0-015 map window chrome color tokens

--- src/features/window-chrome/windowChromeStyle.ts
+++ src/features/window-chrome/windowChromeStyle.ts
@@ -7,4 +7,7 @@ export const windowChromeStyle: WindowChromeStyle = {
   '--km-window-chrome-height': `${windowChromeTokens.height}px`,
   '--km-window-chrome-control-width': `${windowChromeTokens.controlWidth}px`,
   '--km-window-chrome-control-font-size': `${windowChromeTokens.controlFontSize}px`,
+  '--km-window-chrome-control-color': windowChromeTokens.controlColor,
+  '--km-window-chrome-control-hover-bg': windowChromeTokens.controlHoverBackground,
+  '--km-window-chrome-danger-hover-bg': windowChromeTokens.dangerHoverBackground,
 }
