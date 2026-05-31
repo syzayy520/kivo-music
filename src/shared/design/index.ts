b7d18dd416ce9630f631cac09a79a883f09553c8
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-31 10:49:20 +0800

    KIVO-MUSIC-FE-DESIGN-TOKENS-P0-003 add design token exports

---
+++ src/shared/design/index.ts
@@ -0,0 +1,8 @@
+export { designTokenContract } from './tokens'
+export type { DesignTokenContract } from './tokens'
+
+export { playerTokens } from './playerTokens'
+export type { PlayerTokens } from './playerTokens'
+
+export { shellTokens } from './shellTokens'
+export type { ShellTokens } from './shellTokens'
