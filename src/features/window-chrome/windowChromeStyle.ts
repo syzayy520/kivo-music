6b327810b2b6182322637859a94694006e4d0adb
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-31 18:09:36 +0800

    KIVO-MUSIC-WINDOW-CHROME-HOVER-TOKENS-P0-027 consume window chrome hover tokens

--- src/features/window-chrome/windowChromeStyle.ts
+++ src/features/window-chrome/windowChromeStyle.ts
@@ -8,6 +8,7 @@ export const windowChromeStyle: WindowChromeStyle = {
   '--km-window-chrome-control-width': `${windowChromeTokens.controlWidth}px`,
   '--km-window-chrome-control-font-size': `${windowChromeTokens.controlFontSize}px`,
   '--km-window-chrome-control-color': windowChromeTokens.controlColor,
+  '--km-window-chrome-control-hover-color': windowChromeTokens.controlHoverColor,
   '--km-window-chrome-control-hover-bg': windowChromeTokens.controlHoverBackground,
   '--km-window-chrome-danger-hover-bg': windowChromeTokens.dangerHoverBackground,
 }
