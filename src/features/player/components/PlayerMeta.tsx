27f48ef5393a132f3f2de37507e742576cd49491
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-05-31 10:25:40 +0800

    KIVO-MUSIC-FE-I18N-9LOCALE-BASE-P0-002 render player meta translations

--- src/features/player/components/PlayerMeta.tsx
+++ src/features/player/components/PlayerMeta.tsx
@@ -1,19 +1,20 @@
 import './playerMeta.css'
+import { t } from '../../../shared/i18n'
 import { PlayerIcon } from '../playerIcons'
 import { playerData } from '../playerData'
 
 export function PlayerMeta() {
   return (
-    <div className="km-player-meta" aria-label="Player status">
+    <div className="km-player-meta" aria-label={t('player.status.ariaLabel')}>
       <span className="km-player-quality">{playerData.qualityShortLabel}</span>
-      <div className="km-player-volume" aria-label="Volume">
+      <div className="km-player-volume" aria-label={t('player.volume.ariaLabel')}>
         <PlayerIcon name="volume" />
         <div>
           <span style={{ width: `${playerData.state.volume}%` }} />
         </div>
       </div>
       <button type="button" className="km-player-queue">
-        Queue
+        {t('player.queue')}
       </button>
     </div>
   )
