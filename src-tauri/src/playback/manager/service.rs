403ab5dc47ef26e1d6bdfe77f3802ca9d9653ac8
yi <syzayy@gmail.com>
2026-06-03 09:47:17 +0800

    KIVO-AUDIO-GOVERNANCE-P0-019 centralize playback manager service locking

--- src-tauri/src/playback/manager/service.rs
+++ src-tauri/src/playback/manager/service.rs
@@ -6,174 +6,97 @@ use crate::playback::state::PlaybackState;
 use crate::playback::types::{PlaybackTrack, RepeatMode};
 
 impl PlaybackManagerState {
-    pub fn current_state(&self) -> PlaybackState {
+    fn with_manager<T>(&self, read: impl FnOnce(&super::PlaybackManager) -> T) -> T {
         let manager = self
             .manager
             .lock()
             .unwrap_or_else(|poisoned| poisoned.into_inner());
 
-        manager.current_state()
+        read(&manager)
+    }
+
+    fn with_manager_mut<T>(&self, write: impl FnOnce(&mut super::PlaybackManager) -> T) -> T {
+        let mut manager = self
+            .manager
+            .lock()
+            .unwrap_or_else(|poisoned| poisoned.into_inner());
+
+        write(&mut manager)
+    }
+
+    pub fn current_state(&self) -> PlaybackState {
+        self.with_manager(super::PlaybackManager::current_state)
     }
 
     pub fn primary_backend(&self) -> PlaybackBackendDescriptor {
-        let manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.primary_backend()
+        self.with_manager(super::PlaybackManager::primary_backend)
     }
 
     pub fn compatibility_backends(&self) -> Vec<PlaybackBackendDescriptor> {
-        let manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.compatibility_backends()
+        self.with_manager(super::PlaybackManager::compatibility_backends)
     }
 
     pub fn queue(&self) -> PlaybackQueue {
-        let manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.queue()
+        self.with_manager(super::PlaybackManager::queue)
     }
 
     pub fn queue_append(&self, track: PlaybackTrack) -> PlaybackQueue {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.queue_append(track)
+        self.with_manager_mut(move |manager| manager.queue_append(track))
     }
 
     pub fn queue_remove(&self, index: usize) -> PlaybackResult<PlaybackQueue> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.queue_remove(index)
+        self.with_manager_mut(|manager| manager.queue_remove(index))
     }
 
     pub fn queue_set_repeat_mode(&self, repeat_mode: RepeatMode) -> PlaybackQueue {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.queue_set_repeat_mode(repeat_mode)
+        self.with_manager_mut(|manager| manager.queue_set_repeat_mode(repeat_mode))
     }
 
     pub fn queue_set_shuffle(&self, shuffle: bool) -> PlaybackQueue {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.queue_set_shuffle(shuffle)
+        self.with_manager_mut(|manager| manager.queue_set_shuffle(shuffle))
     }
 
     pub fn queue_set_current(&self, index: usize) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.queue_set_current(index)
+        self.with_manager_mut(|manager| manager.queue_set_current(index))
     }
 
     pub fn queue_next(&self) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.queue_next()
+        self.with_manager_mut(super::PlaybackManager::queue_next)
     }
 
     pub fn queue_previous(&self) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.queue_previous()
+        self.with_manager_mut(super::PlaybackManager::queue_previous)
     }
 
     pub fn load(&self, track: PlaybackTrack) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.load(track)
+        self.with_manager_mut(move |manager| manager.load(track))
     }
 
     pub fn play(&self) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.play()
+        self.with_manager_mut(super::PlaybackManager::play)
     }
 
     pub fn pause(&self) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.pause()
+        self.with_manager_mut(super::PlaybackManager::pause)
     }
 
     pub fn resume(&self) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.resume()
+        self.with_manager_mut(super::PlaybackManager::resume)
     }
 
     pub fn stop(&self) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.stop()
+        self.with_manager_mut(super::PlaybackManager::stop)
     }
 
     pub fn seek(&self, position_ms: u64) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.seek(position_ms)
+        self.with_manager_mut(|manager| manager.seek(position_ms))
     }
 
     pub fn set_volume(&self, level: f32) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.set_volume(level)
+        self.with_manager_mut(|manager| manager.set_volume(level))
     }
 
     pub fn set_muted(&self, muted: bool) -> PlaybackResult<PlaybackState> {
-        let mut manager = self
-            .manager
-            .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
-
-        manager.set_muted(muted)
+        self.with_manager_mut(|manager| manager.set_muted(muted))
     }
 }
