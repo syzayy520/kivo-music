537349f393d98f37aa57ef9f2b62745ad25389ab
yi <syzayy@gmail.com>
2026-06-03 09:53:43 +0800

    KIVO-AUDIO-GOVERNANCE-P0-020 restrict native pipeline output test helpers

--- src-tauri/src/playback/native_pipeline_output.rs
+++ src-tauri/src/playback/native_pipeline_output.rs
@@ -1,16 +1,20 @@
 use super::native_pipeline::NativePipeline;
+#[cfg(test)]
 use super::output::{AudioOutputFrame, OutputRuntimeStatus, OutputSettings};
 
 impl NativePipeline {
-    pub fn set_output_settings(&mut self, settings: OutputSettings) {
+    #[cfg(test)]
+    pub(in crate::playback) fn set_output_settings(&mut self, settings: OutputSettings) {
         self.state.output_settings = settings;
     }
 
-    pub fn set_output_status(&mut self, status: OutputRuntimeStatus) {
+    #[cfg(test)]
+    pub(in crate::playback) fn set_output_status(&mut self, status: OutputRuntimeStatus) {
         self.state.output_status = status;
     }
 
-    pub fn note_frame_submitted(&mut self, _frame: &AudioOutputFrame) {
+    #[cfg(test)]
+    pub(in crate::playback) fn note_frame_submitted(&mut self, _frame: &AudioOutputFrame) {
         self.state.output_status.pending_frames =
             self.state.output_status.pending_frames.saturating_add(1);
         self.state.output_status.is_active = true;
